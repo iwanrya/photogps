ea96f976678d560a90faa6b5a3ee5befa1993577
Krishna Dewabrata <krishnadewabrata@hotmail.com>
2023-05-22 13:46:26 +0700

    Display hello world d display hello world!

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //return collection of posts as a resource
+        return response('Hello world!');
+    }
 }
