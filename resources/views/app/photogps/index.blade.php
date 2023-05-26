12d34849462c16e27f08593970ee4302fa557f1b
Krishna Dewabrata <krishnadewabrata@hotmail.com>
2023-05-26 14:23:37 +0700

    Bug fix openstreetview url

--- resources/views/app/photogps/index.blade.php
+++ resources/views/app/photogps/index.blade.php
@@ -15,7 +15,7 @@
         <td>{{ $post->longitude }}</td>
         <td>{{ $post->created_at_formatted }}</td>
         <td>
-            <iframe width="300" height="200" frameborder="1" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox={{ $post->longitude - 0.002 }},{{ $post->latitude - 0.002 }},{{ $post->longitude + 0.002 }},{{ $post->latitude + 0.002 }}&layer=mapnik&marker={{ $post->latitude }},{{ $post->longitude }}" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=-{{ $post->latitude }}&lon={{ $post->longitude }}#map=10/{{ $post->latitude }}/{{ $post->longitude }}">View Larger Map</a></small>
+            <iframe width="300" height="200" frameborder="1" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox={{ $post->longitude - 0.002 }},{{ $post->latitude - 0.002 }},{{ $post->longitude + 0.002 }},{{ $post->latitude + 0.002 }}&layer=mapnik&marker={{ $post->latitude }},{{ $post->longitude }}" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=-{{ $post->latitude }}&mlon={{ $post->longitude }}#map=10/{{ $post->latitude }}/{{ $post->longitude }}">View Larger Map</a></small>
         </td>
     </tr>
     @endforeach
