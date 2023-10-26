f90764c2bce2b21e0566b2b5936c72842918b03f
Krishna Dewabrata <krishnadewabrata@hotmail.com>
2023-10-26 12:43:43 +0700

    fix migrate table company

--- database/migrations/2023_10_26_112209_modify_company_table_add_is_hidden.php
+++ database/migrations/2023_10_26_112209_modify_company_table_add_is_hidden.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->boolean('is_hidden');
+            $table->boolean('is_hidden')->nullable();
         });
     }
 
